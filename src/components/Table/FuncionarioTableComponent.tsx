84d19a5065a2ecfb566bd2de12d3363a69ffcfc7
Anderson <105141966+andersonjhonatan@users.noreply.github.com>
2024-04-02 22:52:28 -0300

    feat: formatando o telephone e datadeaadmissao

--- src/components/Table/FuncionarioTableComponent.tsx
+++ src/components/Table/FuncionarioTableComponent.tsx
@@ -6,6 +6,28 @@ import UserImplemetation from '../../api/Implematation/UserImplemetation'
 const GetUSers = () => {
   const [users, setUsers] = useState<IEmployeesData[]>([])
 
+  const dataDeAdmissaoFormatada = (data: string) => {
+    const date = new Date(data)
+    return date.toLocaleDateString('pt-BR', {
+      day: '2-digit',
+      month: '2-digit',
+      year: 'numeric',
+    })
+  }
+
+  const phoneFormat = (phone: string): string => {
+    const phoneFormatado = phone
+      .replace(/\D/g, '')
+      .replace(/^(\d{0, 2})(\d{5})(\d{4})/, '($1) $2-$3')
+
+    const phoneFormatted = `+${phoneFormatado.slice(0, 2)} (${phoneFormatado.slice(
+      2,
+      4
+    )}) ${phoneFormatado.slice(4, 9)}-${phoneFormatado.slice(9)}`
+
+    return phoneFormatted
+  }
+
   useEffect(() => {
     async function getAllUsers() {
       const userImpleation = new UserImplemetation()
@@ -19,18 +41,20 @@ const GetUSers = () => {
 
   return {
     users,
+    dataDeAdmissaoFormatada,
+    phoneFormat,
   }
 }
 
 const FuncionarioTableComponent = () => {
-  const { users } = GetUSers()
+  const { users, dataDeAdmissaoFormatada, phoneFormat } = GetUSers()
   return (
     <div className="w-full">
       <div className="w-full flex justify-center ">
         <table className="w-full">
           <thead>
-            <tr className="bg-gradient-to-t from-[#594ed2] to-[#5A84C0]  text-[var(--quaternary-color)] text-justify">
-              <th className="text-base leading-[18.75px] py-[14px] rounded-tl-lg pl-[32px]">
+            <tr className="bg-gradient-to-t from-[#594ed2] to-[#5A84C0]  text-[var(--quaternary-color)] text-justify ">
+              <th className="text-base leading-[18.75px] rounded-tl-lg pl-[32px] py-[14px]">
                 FOTO
               </th>
               <th>NOME</th>
@@ -40,21 +64,26 @@ const FuncionarioTableComponent = () => {
             </tr>
           </thead>
           <tbody className="text-justify bg-[var(--quaternary-color)]">
-            {users.map((user) => (
-              <tr key={user.id} className="text-justify border-b">
-                <td className="pl-[32px]  pt-[8px] pb-[7px]">
-                  <img
-                    src={user.image}
-                    alt="user"
-                    className="w-[50px] h-[50px] object-container rounded-full "
-                  />
-                </td>
-                <td>{user.name}</td>
-                <td>{user.job}</td>
-                <td>{user.admission_date}</td>
-                <td>{user.phone}</td>
-              </tr>
-            ))}
+            {users.map(
+              (user) => (
+                console.log(user.phone),
+                (
+                  <tr key={user.id} className="text-justify border-b">
+                    <td className="pl-[32px]  pt-[8px] pb-[7px] h-[49px]">
+                      <img
+                        src={user.image}
+                        alt="user"
+                        className="w-[50px] h-[50px] object-container rounded-full "
+                      />
+                    </td>
+                    <td>{user.name}</td>
+                    <td>{user.job}</td>
+                    <td>{dataDeAdmissaoFormatada(user.admission_date)}</td>
+                    <td>{phoneFormat(user.phone)}</td>
+                  </tr>
+                )
+              )
+            )}
           </tbody>
         </table>
       </div>
